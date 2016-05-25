972f424d8226501a32eb67bcdee40d64940fa020
SynergiaSupport <edson.merida@synergia.com.gt>
2016-05-25 16:58:04 -0600

    Aplicativo SPCComunes

---
+++ SPCComunes/SPCGlobales.vb
@@ -0,0 +1,17 @@
+﻿
+Public Class SPCGlobales
+
+    Public Shared Function TraeEmpresa() As String
+        Return "Empresa: " + TraeDato("Select db_name()")
+    End Function
+
+    Public Shared Function TraeDato(ByVal transactSQL As String) As String
+        Dim retorno As String = String.Empty
+
+        Dim oSQL As New Citi.Synergia.DB.SQL
+        oSQL.ConexionSTR = Citi.Synergia.Locals.Cache.ConexionSTR
+        retorno = oSQL.GetCadena(transactSQL)
+
+        Return retorno
+    End Function
+End Class
