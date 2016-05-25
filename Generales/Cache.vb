6f2161c67881ecfd910b138635e994adfd5e29f0
SynergiaSupport <edson.merida@synergia.com.gt>
2016-05-25 16:47:50 -0600

    Aplicativo Generales

---
+++ Generales/Cache.vb
@@ -0,0 +1,11 @@
+﻿Public Class Cache
+    Public Shared Property Servidor As String
+    Public Shared Property BaseDeDatos As String
+    Public Shared Property Usuario As String
+    Public Shared Property Password As String
+    Public Shared Property UtilizarIdiomas As Boolean
+    Public Shared Property CrearIdioma As Boolean
+
+End Class
+
+
